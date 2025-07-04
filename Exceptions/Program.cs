e9bf5ba407179395047fda3eb29fb737f7e75310
Fsmart2164 <24fs2381@barton.ac.uk>
2025-07-04 15:49:06 +0100

    exceptions

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exceptions
 {
+    public class DivideException : Exception
+    {
+        public DivideException() : base("division error")
+        {
+
+        }
+    }
+    public class SaveFileException : Exception
+    {
+        string savefilename;
+        public SaveFileException(string savefilename) : base("There is an error with the save file called ")
+        {
+            this.savefilename = savefilename;
+            string name = getfilename();
+        }
+        private string getfilename()
+        {
+            return "john";
+        }
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Q1();
+            //Q2();
+        }
+        static void Q2()
+        {
+
+        }
+        static void Q1()
+        {
+            while (true)
+            {
+
+                try
+                {
+                    Console.WriteLine("enter two numbers to divide:");
+                    int x = int.Parse(Console.ReadLine());
+                    int y = int.Parse(Console.ReadLine());
+                    Console.WriteLine(divide(x, y));
+                    break;
+                }
+                catch (DivideException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("error with inputting data");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+        }
+        static int divide(int x, int y)
+        {
+            if (y == 0) throw new DivideException();
+            return x / y;
         }
     }
 }
