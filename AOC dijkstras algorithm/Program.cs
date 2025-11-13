28ab7ba8060060052c12f0f61f7d82878fda288e
Fsmart2164 <24fs2381@barton.ac.uk>
2025-11-13 10:09:29 +0000

    no

---
+++ AOC dijkstras algorithm/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        (int, int) startpoint = (1, 1);
+    }
+    
+}
+internal class Dijkstras
+{
+    static int puzzlenum = 1352;
+    private Vector2 source;
+    private Vector2 destination;
+    private Dictionary<Vector2, int> path = new Dictionary<Vector2, int>();
+    private List<Vector2> Directions = new List<Vector2> { new Vector2(-1,0), new Vector2(1,0), new Vector2(0,-1), new Vector2(0,1)};
+    private HashSet<Vector2> VistedCoords = new HashSet<Vector2>();
+    private Queue<Vector2> visitingCoords = new Queue<Vector2>();
+    public Dijkstras(Vector2 source, Vector2 destination)
+    {
+        this.source = source;
+        this.destination = destination;
+    }
+    public int Run()
+    {
+        bool found = false;
+        while (!found)
+        {
+
+        }
+        return 0;
+    }
+    public void Visit(Vector2 current)
+    {
+        if (VistedCoords.Contains(current)) return;
+
+        VistedCoords.Add(current);
+        foreach (Vector2 direction in Directions)
+        {
+            if (isCubical(direction + current))
+            {
+                visitingCoords.Enqueue(direction + current);
+            }
+        }
+    }
+    static bool isCubical(Vector2 coord)
+    {
+        if ((coord.X < 0) || (coord.Y < 0))
+        {
+            return false;
+        }
+        // x*x + 3*x + 2*x*y + y + y*y
+        int num = int.Parse(((coord.X * coord.X) + (3 * coord.X) + (2 * coord.X + coord.Y) + coord.Y + (coord.Y * coord.Y)).ToString());
+        num += puzzlenum;
+        string binary = Convert.ToString(num, 2);
+        int count = 0;
+        foreach (char c in binary)
+        {
+            if (c == '1')
+            {
+                count++;
+            }
+        }
+        return count % 2 == 0;
+    }
+}
---
+++ Vectors/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Numerics;
+using System.Text;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Vector2 v1 = new Vector2();
+        v1.X = 1;
+        v1.Y = 1;
+        Vector2 v2 = new Vector2();
+        v2.X = 2;
+        v2.Y = 5;
+        Vector2 v3 = -v1 * v2;
+
+        MyVector v4 = new MyVector(new int[] { 2, 3 });
+        MyVector v5 = new MyVector(new int[] { 1, 5 });
+
+        Console.WriteLine(v1);
+        Console.WriteLine(v2);
+        Console.WriteLine(v3);
+        MyVector yes = (-v4);
+        Console.WriteLine(yes);
+        Console.WriteLine(v5.ToString());
+        Console.WriteLine((yes * v5).ToString());
+    }
+}
+
+public class MyVector
+{
+    public int[] values;
+    private int length;
+    public MyVector(int size)
+    {
+        values = new int[size];
+        length = size;
+        WriteNewVector();
+    }
+    public MyVector()
+    {
+        values = new int[2];
+        length = 2;
+        WriteNewVector();
+    }
+    public MyVector(int[] v)
+    {
+        this.length = v.Length;
+        this.values = v;
+    }
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("<");
+        for (int i = 0; i < length; i++)
+        {
+            if (i != 0) sb.Append(", ");
+            sb.Append(values[i]);
+        }
+        sb.Append(">");
+        return sb.ToString();
+    }
+    private void WriteNewVector()
+    {
+        for (int i = 0; i < length; i++)
+        {
+            values[i] = 0;
+        }
+    }
+
+    public static MyVector operator +(MyVector v1, MyVector v2)
+    {
+
+        var vectorpair = SetupOperators(v1,v2);
+
+        int size = vectorpair.Item1.Length;
+        MyVector output = new MyVector(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            output.values[i] = vectorpair.Item1[i] + vectorpair.Item2[i];
+        }
+        return output;
+    }
+    public static bool operator ==(MyVector v1, MyVector v2)
+    {
+        var vectorpair = SetupOperators(v1, v2);
+
+        for (int o = 0; o < vectorpair.Item1.Length; o++)
+        {
+            if (vectorpair.Item1[o] != vectorpair.Item2[o])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static MyVector operator *(MyVector v1, MyVector v2)
+    {
+        var vpair = SetupOperators(v1, v2);
+
+        MyVector ret = new MyVector(vpair.Item1.Length);
+        for (int o = 0; o < vpair.Item1.Length; o++)
+        {
+            ret.values[o] = vpair.Item1[o] * vpair.Item2[o];
+        }
+        return ret;
+    }
+    public static MyVector operator -(MyVector v1)
+    {
+        MyVector ouput = new MyVector(v1.values);
+        for (int o = 0; o < v1.length; o++)
+        {
+            int num = ouput.values[o];
+            int insert = num - num * 2;
+            ouput.values[o] = insert;
+        }
+        return ouput;
+    }
+    public static bool operator !=(MyVector v1, MyVector v2)
+    {
+        if (v1 == v2) return false;
+        return true;
+    }
+    private static (int[], int[]) SetupOperators(MyVector v1, MyVector v2)
+    {
+        (int[], int[]) ret; 
+        int[] firstvalues = v1.values;
+        int[] secondvalues = v2.values;
+        if (firstvalues.Length != secondvalues.Length)
+        {
+            throw new Exception("different lengths");
+        }
+        ret.Item1 = firstvalues;
+        ret.Item2 = secondvalues;
+        return ret;
+    }
+
+    
+}
